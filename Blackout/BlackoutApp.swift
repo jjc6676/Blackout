97c70397a15358e451fd77807e9272174da38443
Jason Choplin <jasonchoplin@gmail.com>
2024-11-17 07:24:15 -0600

    Initial Commit

---
+++ Blackout/BlackoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackoutApp.swift
+//  Blackout
+//
+//  Created by Jason Choplin on 11/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BlackoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blackout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blackout
+//
+//  Created by Jason Choplin on 11/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
